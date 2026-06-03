a0785c7adf5578407e501d6dfa3d1b303159b4bc
João Costa <jvcosta1502@gmail.com>
2026-06-03 00:48:22 -0300

    otimização de classe

--- src/app/services/calendar.py
+++ src/app/services/calendar.py
@@ -5,10 +5,10 @@ from calendar import monthrange
 from app.commands.commands import ask_data_especifica
 
 
-class CalendarioPrograma:
-    def __init__(self) -> None:
-        self.mes_atual = datetime.now().month
+class CalendarioCampanha:
+    def __init__(self, mes: int | None = None) -> None:
         self.ano_atual = datetime.now().year
+        self.mes_atual = mes or datetime.now().month
         self.final_ano = date(self.ano_atual + 1, 12, 31)
         self.ultimo_dia_mes = monthrange(self.ano_atual, self.mes_atual)[1]
         self.feriados = country_holidays("BR", years=self.ano_atual)
@@ -29,31 +29,14 @@ class CalendarioPrograma:
         return calendario_de_dias_uteis
 
 
-class CalendarioEspecificoPrograma:
+class CalendarioEspecificoCampanha(CalendarioCampanha):
     def __init__(self, mes_selecionado: int) -> None:
-        self.ano_atual = datetime.now().year
-        self.final_ano = date(self.ano_atual + 1, 12, 31)
-        self.feriados = country_holidays("BR", years=self.ano_atual)
-        self.ultimo_dia_mes = monthrange(self.ano_atual, mes_selecionado)[1]
-        self.nu_ano_mes = int(date(self.ano_atual, mes_selecionado, 1).strftime("%Y%m"))
-        self.data_fim_periodo_campanha = date(
-            self.ano_atual, mes_selecionado, self.ultimo_dia_mes
-        )
-
-    def dias_uteis(self, start_date: date) -> dict[int, date]:
-        start = start_date or self.inicio_ano
-        index_dia_util = 1
-        calendario_de_dias_uteis = dict()
-        while start <= self.final_ano:
-            if start.weekday() < 5 and start not in self.feriados:
-                calendario_de_dias_uteis[index_dia_util] = start
-                index_dia_util += 1
-            start += timedelta(days=1)
-        return calendario_de_dias_uteis
+        super().__init__(mes=mes_selecionado)
 
 
 def seleciona_data():
     data_especificada = ask_data_especifica()
+
     if data_especificada:
         calendario = CalendarioEspecificoPrograma(data_especificada)
         periodo_campanha_mes_especifico = calendario.data_fim_periodo_campanha
